d6b42e1d68d18e579cefdb7f0c2e559e53a1cb01
RohithaDantala <rohithadantala08@gmail.com>
2025-05-27 09:30:21 +0530

    Add files via upload

---
+++ bus_system.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE busTrackingSystem;
+USE busTrackingSystem;
+
+select * from route;
+select * from bus;
+select * from driver;
+select * from bus_tracking;
+select * from user;
+select * from bus_pass;
